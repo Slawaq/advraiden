87bf5745f76449a60d7d3e5dce768cbb2f6e105a
Vyacheslav Pytel <dereckvk@gmail.com>
2017-04-09 22:12:31 +0300

    Fix welcome message

--- src/handler/redirect/welcome.js
+++ src/handler/redirect/welcome.js
@@ -3,9 +3,24 @@
 const { contact } = require('../../../config.json')
 
 const htmlWelcome = `
+<!DOCTYPE html>
 <html>
+    <head>
+        <style type="text/css">
+            body { width: 100%; margin: 0; float: none; }
+            body { background: white; }
+            body { color: #111; }
+            body { font-family: sans-serif; }
+            a:link { cursor: pointer; text-decoration: underline; color: #06c; }
+            p { font-size: 12pt; } 
+        </style>
+    </head>
     <body>
-        <center>For further information, please contact to <a href="mailto:${contact}">${contact}</a></center>
+        <center>
+            <p>For all cooperation questions please contact: 
+                <a href="mailto:${contact}">${contact}</a>
+            </p>
+        </center>
     </body>
 </html>
 `
